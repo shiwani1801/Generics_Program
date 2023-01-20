1ee5552c5fc06e56f604768e2b1ddea0df335a43
shiwani <shiwanipaunikar1@gmail.com>
2023-01-20 12:31:13 -0800

    Added:find the max float from 3 element

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.generics;
 
+
 public class TestMaximum {
 
     public static void main(String[] args) {
 
-        Integer number1 = 2;
-        Integer number2 = 3;
-        Integer number3 = 5;
-        Integer maximumNum = testMax(number1,number2,number3);
+        Float number1 = 2.5f;
+        Float number2 = 3.1f;
+        Float number3 = 1.19f;
+        Float maximumNum = testMax(number1,number2,number3);
         System.out.println("Maximum number is : "+maximumNum);
     }
 
-    public static Integer testMax(Integer number1, Integer number2, Integer number3) {
+    public static float testMax(Float number1, Float number2, Float number3) {
 
-        Integer maximumNumber = number1;
+        Float maximumNumber = number1;
         if(number2.compareTo(maximumNumber) > 0 )
         {
             maximumNumber = number2;
