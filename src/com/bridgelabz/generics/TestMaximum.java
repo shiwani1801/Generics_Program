100d4f4c40dd73a9d3a5ec00adf77a68de33f175
shiwani <shiwanipaunikar1@gmail.com>
2023-01-20 12:28:22 -0800

    Added:find the max integer from 3 element

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.generics;
 
 public class TestMaximum {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Generic program ");
+
+        Integer number1 = 2;
+        Integer number2 = 3;
+        Integer number3 = 5;
+        Integer maximumNum = testMax(number1,number2,number3);
+        System.out.println("Maximum number is : "+maximumNum);
+    }
+
+    public static Integer testMax(Integer number1, Integer number2, Integer number3) {
+
+        Integer maximumNumber = number1;
+        if(number2.compareTo(maximumNumber) > 0 )
+        {
+            maximumNumber = number2;
+        }
+        if(number3.compareTo(maximumNumber) > 0 )
+        {
+            maximumNumber = number3;
+        }
+        return maximumNumber;
     }
 }
